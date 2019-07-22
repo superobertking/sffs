7dc78bf269f95b2128741741406a8f5aa0ff7d90
robertking <superobertking@icloud.com>
2019-07-22 22:31:00 +0800

    refine server error handling

--- src/sffsserver.rs
+++ src/sffsserver.rs
@@ -34,48 +34,34 @@ impl SFFSServer {
     }
 }
 
+macro_rules! to_future {
+    ($sink:expr, $res:expr) => {
+        match $res {
+            Some(res) => $sink.success(res),
+            None => $sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)),
+        }
+    };
+}
+
 macro_rules! reply {
     ($ctx:expr, $req:expr, $fut:expr) => {
         $ctx.spawn($fut.map_err(move |e| eprintln!("failed to reply {:?}: {:?}", $req, e)));
     };
 }
 
-impl Sffs for SFFSServer {
-    fn getdir(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::String>) {
-        let mut res: Option<String> = None;
-        if let Ok(cwd) = std::env::current_dir() {
-            if let Ok(cwd) = cwd.into_os_string().into_string() {
-                res = Some(cwd);
-            }
-        }
-        let f = match res {
-            Some(cwd) => sink.success(cwd.into()),
-            None => sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)),
-        };
-        reply!(ctx, req, f);
+impl SFFSServer {
+    fn getdir(&mut self) -> Option<sffs::String> {
+        Some(env::current_dir().ok()?.into_os_string().into_string().ok()?.into())
     }
-
-    fn changedir(&mut self, ctx: RpcContext, req: sffs::String, sink: UnarySink<sffs::Boolean>) {
-        let res = env::set_current_dir(req.get_value()).is_ok();
-        reply!(ctx, req, sink.success(res.into()));
+    fn changedir(&mut self, req: &sffs::String) -> Option<sffs::Boolean> {
+        Some(env::set_current_dir(req.get_value()).is_ok().into())
     }
-
-    fn filecount(&mut self, ctx: RpcContext, req: sffs::ListOption, sink: UnarySink<sffs::Int64>) {
-        let f = match fs::read_dir(".") {
-            Ok(dir) => sink.success((dir.filter(|e| e.is_ok()).count() as i64).into()),
-            Err(_) => sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)),
-        };
-        reply!(ctx, req, f);
+    fn filecount(&mut self, req: &sffs::ListOption) -> Option<sffs::Int64> {
+        // TODO: filter
+        Some((fs::read_dir(".").ok()?.filter(|e| e.is_ok()).count() as i64).into())
     }
-
-    fn openlist(&mut self, ctx: RpcContext, req: sffs::ListRequest, sink: UnarySink<sffs::Boolean>) {
-        let mut guard = match self.0.opendir.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
+    fn openlist(&mut self, req: &sffs::ListRequest) -> Option<sffs::Boolean> {
+        let mut guard = self.0.opendir.lock().ok()?;
 
         let res = if (*guard).is_some() {
             false
@@ -87,88 +73,49 @@ impl Sffs for SFFSServer {
         };
         drop(guard); // release lock
 
-        reply!(ctx, req, sink.success(res.into()));
+        Some(res.into())
     }
+    fn nextlist(&mut self) -> Option<sffs::DirEntry> {
+        // TODO: filter
+        let mut guard = self.0.opendir.lock().ok()?;
 
-    fn nextlist(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::DirEntry>) {
-        let mut guard = match self.0.opendir.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
+        let (ref mut dir, ref path, ref mut next) = guard.as_mut()?;
 
-        let f = match *guard {
-            Some((ref mut dir, ref path, ref mut next)) => match *next {
-                NextEntry::File => loop {
-                    match dir.next() {
-                        Some(entry) => {
-                            if let Ok(e) = entry {
-                                if let Ok(e) = sffs::DirEntry::try_from(e) {
-                                    break sink.success(e);
-                                }
-                            }
-                        }
-                        None => break sink.success(Default::default()),
+        let mut getnext = || -> Option<sffs::DirEntry> {
+            // There will definitely be one entry, even may be empty.
+            Some(loop {
+                match *next {
+                    // next is still file
+                    NextEntry::File => match dir.next() {
+                        Some(entry) => break sffs::DirEntry::try_from(entry.ok()?).ok()?,
+                        None => break sffs::DirEntry::default(),
+                    },
+                    NextEntry::Dot => {
+                        *next = NextEntry::DotDot;
+                        let meta = File::open(path.join(".")).ok()?.metadata().ok()?;
+                        break (".".to_owned(), meta).try_into().ok()?;
                     }
-                },
-                NextEntry::Dot => {
-                    // FIXME: this unwrap
-                    *next = NextEntry::DotDot;
-                    let mut path = path.clone();
-                    path.push(".");
-                    let dir = File::open(path).unwrap();
-                    if let Ok(e) = (".".to_owned(), dir.metadata().unwrap()).try_into() {
-                        sink.success(e)
-                    } else {
-                        sink.success(Default::default())
+                    NextEntry::DotDot => {
+                        *next = NextEntry::File;
+                        let meta = File::open(path.join("..")).ok()?.metadata().ok()?;
+                        break ("..".to_owned(), meta).try_into().ok()?;
                     }
                 }
-                NextEntry::DotDot => {
-                    // FIXME: this unwrap
-                    *next = NextEntry::File;
-                    let mut path = path.clone();
-                    path.push("..");
-                    let dir = File::open(path).unwrap();
-                    if let Ok(e) = ("..".to_owned(), dir.metadata().unwrap()).try_into() {
-                        sink.success(e)
-                    } else {
-                        sink.success(Default::default())
-                    }
-                }
-            },
-            None => sink.fail(RpcStatus::new(RpcStatusCode::InvalidArgument, None)),
+            })
         };
-        drop(guard); // release lock
-
-        reply!(ctx, req, f);
-    }
-
-    fn closelist(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::Boolean>) {
-        let mut guard = match self.0.opendir.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
+        loop {
+            let entry = getnext();
+            if entry.is_some() {
+                return entry;
             }
-        };
-
-        let res = (*guard).is_some();
-        *guard = None;
-        drop(guard); // release lock
-
-        reply!(ctx, req, sink.success(res.into()));
+        }
     }
-
-    fn openfiletoread(&mut self, ctx: RpcContext, req: sffs::String, sink: UnarySink<sffs::Boolean>) {
-        let mut guard = match self.0.openfile.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
+    fn closelist(&mut self) -> Option<sffs::Boolean> {
+        let mut guard = self.0.opendir.lock().ok()?;
+        Some((*guard).take().is_some().into())
+    }
+    fn openfiletoread(&mut self, req: &sffs::String) -> Option<sffs::Boolean> {
+        let mut guard = self.0.openfile.lock().ok()?;
 
         let res = if (*guard).is_some() {
             false
@@ -176,19 +123,10 @@ impl Sffs for SFFSServer {
             *guard = File::open(req.get_value()).ok();
             (*guard).is_some()
         };
-        drop(guard); // release lock
-
-        reply!(ctx, req, sink.success(res.into()));
+        Some(res.into())
     }
-
-    fn openfiletowrite(&mut self, ctx: RpcContext, req: sffs::String, sink: UnarySink<sffs::Boolean>) {
-        let mut guard = match self.0.openfile.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
+    fn openfiletowrite(&mut self, req: &sffs::String) -> Option<sffs::Boolean> {
+        let mut guard = self.0.openfile.lock().ok()?;
 
         let res = if (*guard).is_some() {
             false
@@ -196,91 +134,89 @@ impl Sffs for SFFSServer {
             *guard = File::create(req.get_value()).ok();
             (*guard).is_some()
         };
-        drop(guard); // release lock
-
-        reply!(ctx, req, sink.success(res.into()));
+        Some(res.into())
     }
+    fn nextread(&mut self) -> Option<sffs::Block> {
+        let mut guard = self.0.openfile.lock().ok()?;
 
+        let ref mut file = guard.as_mut()?;
+
+        let mut buf = vec![0u8; MAX_BLOCK_SIZE];
+        let len = file.read(&mut buf).unwrap_or(0);
+        buf.truncate(len);
+        Some(buf.into())
+    }
+    fn nextwrite(&mut self, req: &sffs::Block) -> Option<sffs::Boolean> {
+        let mut guard = self.0.openfile.lock().ok()?;
+
+        let ref mut file = guard.as_mut()?;
+
+        let res = file.write(req.get_data()).is_ok();
+        Some(res.into())
+    }
+    fn randomread(&mut self, req: &sffs::Range) -> Option<sffs::Block> {
+        let mut guard = self.0.openfile.lock().ok()?;;
+
+        let ref mut file = guard.as_mut()?;
+
+        let mut buf = vec![0u8; req.get_count() as usize];
+        let len = file.read_at(&mut buf, req.get_start() as u64).unwrap_or(0);
+        buf.truncate(len);
+        Some(buf.into())
+    }
+    fn closefile(&mut self) -> Option<sffs::Boolean> {
+        let mut guard = self.0.openfile.lock().ok()?;;
+        Some((*guard).take().is_some().into())
+    }
+}
+
+impl Sffs for SFFSServer {
+    #[inline]
+    fn getdir(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::String>) {
+        reply!(ctx, req, to_future!(sink, self.getdir()));
+    }
+    #[inline]
+    fn changedir(&mut self, ctx: RpcContext, req: sffs::String, sink: UnarySink<sffs::Boolean>) {
+        reply!(ctx, req, to_future!(sink, self.changedir(&req)));
+    }
+    #[inline]
+    fn filecount(&mut self, ctx: RpcContext, req: sffs::ListOption, sink: UnarySink<sffs::Int64>) {
+        reply!(ctx, req, to_future!(sink, self.filecount(&req)));
+    }
+    #[inline]
+    fn openlist(&mut self, ctx: RpcContext, req: sffs::ListRequest, sink: UnarySink<sffs::Boolean>) {
+        reply!(ctx, req, to_future!(sink, self.openlist(&req)));
+    }
+    #[inline]
+    fn nextlist(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::DirEntry>) {
+        reply!(ctx, req, to_future!(sink, self.nextlist()));
+    }
+    #[inline]
+    fn closelist(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::Boolean>) {
+        reply!(ctx, req, to_future!(sink, self.closelist()));
+    }
+    #[inline]
+    fn openfiletoread(&mut self, ctx: RpcContext, req: sffs::String, sink: UnarySink<sffs::Boolean>) {
+        reply!(ctx, req, to_future!(sink, self.openfiletoread(&req)));
+    }
+    #[inline]
+    fn openfiletowrite(&mut self, ctx: RpcContext, req: sffs::String, sink: UnarySink<sffs::Boolean>) {
+        reply!(ctx, req, to_future!(sink, self.openfiletowrite(&req)));
+    }
+    #[inline]
     fn nextread(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::Block>) {
-        let mut guard = match self.0.openfile.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
-
-        let f = match *guard {
-            Some(ref mut file) => {
-                let mut buf = vec![0u8; MAX_BLOCK_SIZE];
-                let len = file.read(&mut buf).unwrap_or(0);
-                buf.truncate(len);
-                sink.success(buf.into())
-            }
-            None => sink.fail(RpcStatus::new(RpcStatusCode::InvalidArgument, None)),
-        };
-        drop(guard); // release lock
-
-        reply!(ctx, req, f);
+        reply!(ctx, req, to_future!(sink, self.nextread()));
     }
-
+    #[inline]
     fn nextwrite(&mut self, ctx: RpcContext, req: sffs::Block, sink: UnarySink<sffs::Boolean>) {
-        let mut guard = match self.0.openfile.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
-
-        let f = match *guard {
-            Some(ref mut file) => {
-                let res = file.write(req.get_data()).is_ok();
-                sink.success(res.into())
-            }
-            None => sink.fail(RpcStatus::new(RpcStatusCode::InvalidArgument, None)),
-        };
-        drop(guard); // release lock
-
-        reply!(ctx, req, f);
+        reply!(ctx, req, to_future!(sink, self.nextwrite(&req)));
     }
-
+    #[inline]
     fn randomread(&mut self, ctx: RpcContext, req: sffs::Range, sink: UnarySink<sffs::Block>) {
-        let mut guard = match self.0.openfile.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
-
-        let f = match *guard {
-            Some(ref mut file) => {
-                let mut buf = vec![0u8; req.get_count() as usize];
-                let len = file.read_at(&mut buf, req.get_start() as u64).unwrap_or(0);
-                buf.truncate(len);
-                sink.success(buf.into())
-            }
-            None => sink.fail(RpcStatus::new(RpcStatusCode::InvalidArgument, None)),
-        };
-        drop(guard); // release lock
-
-        reply!(ctx, req, f);
+        reply!(ctx, req, to_future!(sink, self.randomread(&req)));
     }
-
+    #[inline]
     fn closefile(&mut self, ctx: RpcContext, req: sffs::Void, sink: UnarySink<sffs::Boolean>) {
-        let mut guard = match self.0.openfile.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                reply!(ctx, req, sink.fail(RpcStatus::new(RpcStatusCode::Internal, None)));
-                return;
-            }
-        };
-
-        let res = (*guard).is_some();
-        *guard = None;
-        drop(guard); // release lock
-
-        reply!(ctx, req, sink.success(res.into()));
+        reply!(ctx, req, to_future!(sink, self.closefile()));
     }
 }
